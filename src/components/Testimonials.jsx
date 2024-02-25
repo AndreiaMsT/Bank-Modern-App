8e6b409089ce54c9ad9096faaccbfbc98d6c692b
AndreiaMsT <am.teixeira4@gmail.com>
2024-02-25 21:49:20 +0000

    done Testimonials component

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -8,7 +8,7 @@ const Testimonials = () => {
       id="clients"
       className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}
     >
-      <div />
+      <div className="absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient"/>
       <div className="w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1] ">
         <h2 className={styles.heading2}>
           What People are <br className="sm:block hidden" /> saying about us
@@ -23,7 +23,7 @@ const Testimonials = () => {
 
       <div className="flex flex-wrap sm:justify-start justify-center w-full feedback-container relative z-[1] ">
 {feedback.map((f,index)=>(
-  <FeedbackCard key={f.id} {...f}/>
+  <FeedbackCard key={f.id} {...f} index={index}/>
 ))}
       </div>
     </section>
